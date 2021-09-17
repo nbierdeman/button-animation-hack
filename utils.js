cdc9780d29d87285623d3837cc5d787a21c094c9
gjopa <gjopa@paypal.com>
2021-09-17 15:52:28 -0500

    Add support for both flats and sharps (#4)

--- utils.js
+++ utils.js
@@ -1,9 +1,23 @@
-const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+const notes = [
+    ["C"],
+    ["C#", "Db"],
+    ["D"],
+    ["D#", "Eb"],
+    ["E"],
+    ["F"],
+    ["F#", "Gb"],
+    ["G"],
+    ["G#", "Ab"],
+    ["A"],
+    ["A#", "Bb"],
+    ["B"]
+];
 
 const baseNote = {
-    name: "C",
+    name: "A",
     octave: 4,
-    frequency: 261.6255
+    frequency: 440,
+    halfStepsFromC4: 9
 }
 
 /*
@@ -12,17 +26,33 @@ const baseNote = {
  * @param {string} note - the note letter and octave
  *
  * Examples:
+ * `frequencyFromNote("D4")` => 293.66
  * `frequencyFromNote("A4")` => 440
- * `frequencyFromNote("D4")` => 587.33
  * `frequencyFromNote("A5")` => 880
  *
  * Source: https://pages.mtu.edu/~suits/NoteFreqCalcs.html
 */
+
 function frequencyFromNote(note) {
     const { noteName, octave } = parseNote(note);
-    const steps = notes.indexOf(noteName) + ((octave - baseNote.octave) * notes.length);
-    const frequency = baseNote.frequency * Math.pow(Math.pow(2, (1/12)), steps);
-    return Math.round((frequency + Number.EPSILON) * 100) / 100;
+    const noteIndexOffset = getNoteIndex(noteName) - baseNote.halfStepsFromC4;
+
+    const halfSteps = noteIndexOffset + ((octave - baseNote.octave) * notes.length);
+    const frequency = baseNote.frequency * Math.pow(Math.pow(2, (1/12)), halfSteps);
+    return frequency;
+}
+
+function getNoteIndex(noteName) {
+    let noteIndex;
+
+    notes.find((noteArray, index) => {
+        if (noteArray.includes(noteName)) {
+            noteIndex = index;
+            return true;
+        }
+    });
+
+    return noteIndex;
 }
 
 function parseNote(note) {
@@ -31,9 +61,8 @@ function parseNote(note) {
     }
 
     let [noteName, octave] = note.split(/([0-9]+)/);
-    noteName = noteName.toUpperCase();
 
-    if (!notes.includes(noteName)) {
+    if (getNoteIndex(noteName) === undefined) {
         throw new Error(`"${noteName}" not found in the list: ${notes}`);
     }
 
@@ -48,3 +77,22 @@ function parseNote(note) {
         octave
     };
 }
+
+function runTests() {
+    function assertEquals(num1, num2) {
+        if (num1.toFixed(2) !== num2.toFixed(2)) {
+            throw new Error(`the number ${num1.toFixed(2)} does not equal ${num2.toFixed(2)}`);
+        }
+    }
+
+    assertEquals(frequencyFromNote("A3"), 220);
+    assertEquals(frequencyFromNote("A4"), 440);
+    assertEquals(frequencyFromNote("A5"), 880);
+
+    assertEquals(frequencyFromNote("C4"), 261.63);
+    assertEquals(frequencyFromNote("D4"), 293.66);
+    assertEquals(frequencyFromNote("A#4"), 466.16);
+    assertEquals(frequencyFromNote("Ab4"), 415.30);
+}
+
+runTests();
